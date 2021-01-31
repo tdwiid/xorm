c715286172aa4050393c3abd2b3bb256f4dc6aa5
Dwicahya Sulistyawan <dwicahya.sulistyawan@gmail.com>
2021-01-31 14:05:06 +0700

    Update update.go

--- internal/statements/update.go
+++ internal/statements/update.go
@@ -11,11 +11,11 @@ import (
 	"reflect"
 	"time"
 
-	"xorm.io/xorm/convert"
-	"xorm.io/xorm/dialects"
-	"xorm.io/xorm/internal/json"
-	"xorm.io/xorm/internal/utils"
-	"xorm.io/xorm/schemas"
+	"github.com/tdwiid/xorm/convert"
+	"github.com/tdwiid/xorm/dialects"
+	"github.com/tdwiid/xorm/internal/json"
+	"github.com/tdwiid/xorm/internal/utils"
+	"github.com/tdwiid/xorm/schemas"
 )
 
 func (statement *Statement) ifAddColUpdate(col *schemas.Column, includeVersion, includeUpdated, includeNil,
